e413716120b3a9974f95ab3e0137fad8aca6a32b
Alexa <alexandraws29@outlook.com>
2020-08-13 17:40:41 -0600

    created loop to fight all enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
+  
   // Alert users that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -66,22 +65,16 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
 
 
-  // check player's health
-  if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-  } 
-  else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-  }
 
-  // check enemy's health
-  if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
-  } 
-  else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-  }
+  // Game States
+  // "WIN" - Player robot has defeated all enemy robots
+  //    * Fight all enemy robots
+  //    * Defeat each enemy robot
+  // "LOSE" - Player robot's health is zero or less
+
 
 };
 
-fight(); 
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
